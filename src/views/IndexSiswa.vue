28409d78be3c8ef19624e80410f940ad76fc0349
FavianIbra <ibra.yanuarta@gmail.com>
2023-02-28 13:38:27 +0700

    add modal siswa

--- src/views/IndexSiswa.vue
+++ src/views/IndexSiswa.vue
@@ -1,62 +1,99 @@
 <template>
     <div>
-        <navbar-component></navbar-component>
-        <sidebar-component></sidebar-component>
-        <div class="content-wrapper">            
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0">Data Anggota</h1>
-                        </div>          
-                    </div>
+    <!-- Button trigger modal -->
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Tambah Siswa</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form @submit.prevent="save">
+            <label for="nama" class="form-label">Nama Siswa:</label>
+            <input type="text" class="form-control" v-model="siswa.nama_siswa" id="nama" autocomplete="off" placeholder="Masukkan nama..">
+            <label for="tgl_lahir" class="form-label">Date:</label>
+            <input type="date" class="form-control" v-model="siswa.tanggal_lahir" id="tgl_lahir" autocomplete="off">
+            <label for="gender" class="form-label">Gender: </label> <br>
+            <div class="btn-group btn-group-toggle" id="gender" data-toggle="buttons">
+                <label class="btn" style="background-color: aqua;">
+                    <input type="radio" value="L" v-model="siswa.gender"> Laki-laki
+                </label>
+                <label class="btn" style="background-color: chartreuse;">
+                    <input type="radio" value="P" v-model="siswa.gender"> Perempuan
+                </label>
+            </div><br>
+            <label for="kelas">Kelas:</label>
+            <select v-model="siswa.id_kelas" id="kelas" class="form-control">
+                <option v-for="k in kelas" :key="k.id_kelas" :value="k.id_kelas">{{ k.nama_kelas }}</option>
+            </select>
+            <label for="alamat" class="form-label">Alamat:</label>
+            <input type="text" class="form-control" v-model="siswa.alamat" id="alamat" autocomplete="off">
+            <br>
+            <input type="submit" class="btn btn-primary">
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<navbar-component></navbar-component>
+<sidebar-component></sidebar-component>
+<div class="content-wrapper">            
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Data Anggota</h1>
                 </div>
             </div>
-            <div class="content">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="card card-primary card-outline">
-                                <div class="card-body">
-                                    <router-link class="btn mb-2" style="background-color: #F0EEED;" to="">
-                                        <i class="fas fa-plus"></i> Tambah
-                                    </router-link>
-                                    <table class="table table-bordered">
-                                        <thead>
-                                            <tr>
-                                            <th style="width: 10px">#</th>
-                                            <th>Nama Lengkap</th>
-                                            <th>Kelas</th>
-                                            <th>Gender</th>
-                                            <th>Tanggal Lahir</th>
-                                            <th>Aksi</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr v-for="s in siswa" :key="s.id_siswa">
-                                                <td>{{ s.id_siswa }}</td>
-                                                <td>{{ s.nama_siswa }}</td>
-                                                <td>{{ s.nama_kelas }}</td>
-                                                <td>{{ s.gender }}</td>
-                                                <td>{{ s.tanggal_lahir }}</td>
-                                               
-                                                <td>
-                                                    <div class="btn-group">                                                       
-                                                        <router-link class="btn" style ="background-color: #609EA2 ;" :to="{ name : 'editsiswa' , params : { id : s.id } }">Edit</router-link>  
-                                                        <button type="button" @click="hapus(s.id)" class="btn" style="background-color: #C92C6D;">Hapus</button>                                                      
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
+        </div>
+    </div>
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card card-primary card-outline">
+                        <div class="card-body">
+                            <button type="button" class="btn btn-primary mb-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                Tambah Siswa
+                            </button>
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 10px">#</th>
+                                        <th>Nama Lengkap</th>
+                                        <th>Kelas</th>
+                                        <th>Gender</th>
+                                        <th>Tanggal Lahir</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="s in siswa" :key="s.id_siswa">
+                                        <td>{{ s.id_siswa }}</td>
+                                        <td>{{ s.nama_siswa }}</td>
+                                        <td>{{ s.nama_kelas }}</td>
+                                        <td>{{ s.gender }}</td>
+                                        <td>{{ s.tanggal_lahir }}</td>
+                                        <td>
+                                            <div class="btn-group">                                                       
+                                                <router-link class="btn" style ="background-color: #609EA2 ;" :to="{ name : 'editsiswa' , params : { id : s.id } }">Edit</router-link>  
+                                                <button type="button" @click="hapus(s.id)" class="btn" style="background-color: #C92C6D;">Hapus</button>                                                      
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+</div>
 </template>
 <script>
 
@@ -74,11 +111,13 @@ export default {
     },
     data() {
         return {
-            siswa : {}
+            siswa : {},
+            kelas: {},
         }
     },
     created() {
         this.DataSiswa();
+        this.getKelas();
     },
     methods : {
         DataSiswa(){
@@ -89,6 +128,26 @@ export default {
                     this.siswa = data;
                 }
             );
+        },
+        getKelas(){
+            axios.get('http://localhost:8000/api/getkelas')
+            .then(
+                ({data}) => {
+                    console.log(data);
+                    this.kelas = data
+                }
+            )
+        },
+        save(){
+            this.save_data();
+        },
+        save_data(){
+            axios.post('http://localhost:8000/api/createsiswa' , this.siswa, this.kelas)
+            .then(
+                ({data}) => {
+                    this.siswa = data
+                }
+            )
         }
     }
 }
